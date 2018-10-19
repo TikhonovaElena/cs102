87494faf590fdc43869705c91307279081d019e6
Elena <lenatika0@gmail.com>
2018-10-19 20:07:17 +0300

    Реализована функция encrypt_vigener()

--- Рабочий стол/Программирование/practice/homework01/vigener.py
+++ Рабочий стол/Программирование/practice/homework01/vigener.py
@@ -1,1 +1,34 @@
+def encrypt_vigenere(plaintext, keyword):
+    """
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
+    # PUT YOUR CODE HERE
+    ciphertext = ''
+    pos = 0
+    for char in plaintext:
+        if 65 <= ord(char) <= 90:
+            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)])))%26+65)
+        elif 97 <= ord(char) <= 122:
+            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)])))%26+97)
+        else:
+            ciphertext += char
+        pos += 1
+    return ciphertext
 
+
+def decrypt_vigenere(ciphertext, keyword):
+    """
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
+    # PUT YOUR CODE HERE
+    return plaintext
