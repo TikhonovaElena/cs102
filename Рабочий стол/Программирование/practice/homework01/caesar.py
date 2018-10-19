c1c0782e6c9a8b63d12a627eedece52718b92759
Elena <lenatika0@gmail.com>
2018-10-19 19:42:41 +0300

    Теперь в функциях encrypt_caesar() and decrypt_caesar() произвольный сдвиг

--- Рабочий стол/Программирование/practice/homework01/caesar.py
+++ Рабочий стол/Программирование/practice/homework01/caesar.py
@@ -1,11 +1,11 @@
 
-def encrypt_caesar(plaintext):
+def encrypt_caesar(plaintext, shift):
     """
-    >>> encrypt_caesar("PYTHON")
+    >>> encrypt_caesar("PYTHON,3")
     'SBWKRQ'
-    >>> encrypt_caesar("python")
+    >>> encrypt_caesar("python, 3")
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
+    >>> encrypt_caesar("Python3.6, 3")
     'Sbwkrq3.6'
     >>> encrypt_caesar("")
     ''
@@ -13,22 +13,22 @@ def encrypt_caesar(plaintext):
     ciphertext = ''
     for char in plaintext:
         if  65 <= ord(char) <= 90:
-            ciphertext += chr((ord(char)+3-65)%26+65)
+            ciphertext += chr((ord(char)+shift-65)%26+65)
         elif 97 <= ord(char) <= 122:
-            ciphertext += chr((ord(char)+3-97)%26+97)
+            ciphertext += chr((ord(char)+shift-97)%26+97)
         else:
             ciphertext += char
 
     return ciphertext
 
 
-def decrypt_caesar(ciphertext):
+def decrypt_caesar(ciphertext, shift):
     """
-    >>> decrypt_caesar("SBWKRQ")
+    >>> decrypt_caesar("SBWKRQ, 3")
     'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
+    >>> decrypt_caesar("sbwkrq, 3")
     'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
+    >>> decrypt_caesar("Sbwkrq3.6, 3")
     'Python3.6'
     >>> decrypt_caesar("")
     ''
@@ -36,9 +36,9 @@ def decrypt_caesar(ciphertext):
     plaintext = ''
     for char in ciphertext:
         if 65 <= ord(char) <= 90:
-            plaintext += chr((ord(char)-3-65+26)%26+65)
+            plaintext += chr((ord(char)-shift-65+26)%26+65)
         elif 97 <= ord(char) <= 122:
-            plaintext += chr((ord(char)-3-97+26)%26+97)
+            plaintext += chr((ord(char)-shift-97+26)%26+97)
         else:
             plaintext += char
     return plaintext
