a3810116cf604bbae202968d850c119d7f7cd407
Elena <lenatika0@gmail.com>
2018-10-19 20:10:09 +0300

    Реализована функция decrypt_vigener() Исправлена функция encrypt_vigener()

--- Рабочий стол/Программирование/practice/homework01/vigener.py
+++ Рабочий стол/Программирование/practice/homework01/vigener.py
@@ -12,9 +12,9 @@ def encrypt_vigenere(plaintext, keyword):
     pos = 0
     for char in plaintext:
         if 65 <= ord(char) <= 90:
-            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)])))%26+65)
+            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)]))-65)%26+65)
         elif 97 <= ord(char) <= 122:
-            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)])))%26+97)
+            ciphertext += chr((ord(char)+(ord(keyword[pos%len(keyword)]))-97)%26+97)
         else:
             ciphertext += char
         pos += 1
@@ -31,4 +31,14 @@ def decrypt_vigenere(ciphertext, keyword):
     'ATTACKATDAWN'
     """
     # PUT YOUR CODE HERE
+    plaintext = ''
+    pos = 0
+    for char in ciphertext:
+        if 65 <= ord(char) <= 90:
+            plaintext += chr((ord(char)-(ord(keyword[pos%len(keyword)]))+26-65)%26+65)
+        elif 97 <= ord(char) <= 122:
+            plaintext += chr((ord(char)-(ord(keyword[pos%len(keyword)]))+26-97)%26+97)
+        else:
+            plaintext += char
+        pos += 1
     return plaintext
