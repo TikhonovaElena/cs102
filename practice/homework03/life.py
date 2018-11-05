24855b1ed27dcadd97b989335cadf4482562f00c
Elena <lenatika0@gmail.com>
2018-11-05 18:06:41 +0300

    Добавлены аннотации ко всем функциям

--- practice/homework03/life.py
+++ practice/homework03/life.py
@@ -1,11 +1,14 @@
 import pygame
 from pygame.locals import *
 import random
+from typing import List, Tuple
 
 
 class GameOfLife:
 
-    def __init__(self, width=640, height=480, cell_size=10, speed=10):
+    def __init__(
+            self, width: int=640, height: int=480,
+            cell_size: int=10, speed: int=10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -22,7 +25,7 @@ class GameOfLife:
         # Скорость протекания игры
         self.speed = speed
 
-    def draw_grid(self):
+    def draw_grid(self) -> None:
         """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(
@@ -31,7 +34,7 @@ class GameOfLife:
             pygame.draw.line(
                 self.screen, pygame.Color('black'), (0, y), (self.width, y))
 
-    def run(self):
+    def run(self) -> None:
         """ Запустить игру """
         pygame.init()
         clock = pygame.time.Clock()
@@ -39,7 +42,6 @@ class GameOfLife:
         self.screen.fill(pygame.Color('white'))
 
         # Создание списка клеток
-        # PUT YOUR CODE HERE
         self.clist = self.cell_list(randomize=True)
 
         running = True
@@ -52,13 +54,11 @@ class GameOfLife:
             # Отрисовка списка клеток
             # Выполнение одного шага игры (обновление состояния ячеек)
             self.draw_cell_list(self.update_cell_list(self.clist))
-            # PUT YOUR CODE HERE
-
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
 
-    def cell_list(self, randomize=True):
+    def cell_list(self, randomize: bool=True) -> List[List[int]]:
         """ Создание списка клеток.
 
         :param randomize: Если True, то создается список клеток, где
@@ -67,14 +67,13 @@ class GameOfLife:
         """
         self.clist = []
         if randomize:
-            for row in range(self.cell_height):
-                line = []
-                for col in range(self.cell_width):
-                    line.append(random.randint(0, 1))
-                self.clist.append(line)
+            self.clist = [
+                [random.randint(0, 1) for col in range(self.cell_width)]
+                for row in range(self.cell_height)
+            ]
         return self.clist
 
-    def draw_cell_list(self, clist):
+    def draw_cell_list(self, clist: List[List[int]]) -> None:
         """ Отображение списка клеток
 
         :param rects:
@@ -86,11 +85,11 @@ class GameOfLife:
                     self.cell_size * col, self.cell_size * row,
                     self.cell_size, self.cell_size)
                 color = (
-                    pygame.Color('green') if
-                    clist[row][col] == 1 else pygame.Color('white'))
+                    pygame.Color('green') if clist[row][col] == 1 else
+                    pygame.Color('white'))
                 pygame.draw.rect(self.screen, color, Rect)
 
-    def get_neighbours(self, cell):
+    def get_neighbours(self, cell: Tuple[int, int]) -> int:
         """ Вернуть список соседей для указанной ячейки
 
         :param cell: Позиция ячейки в сетке, задается кортежем вида (row, col)
@@ -99,8 +98,8 @@ class GameOfLife:
         counter = 0
         neighbours = [
             [0 for col in range(len(self.clist[0]))]
-            for row in range(len(self.clist))]
-
+            for row in range(len(self.clist))
+        ]
         for row in range(cell[0] - 1, cell[0] + 2):
             for col in range(cell[1] - 1, cell[1] + 2):
                 if (0 <= row < len(self.clist)) and
@@ -108,10 +107,9 @@ class GameOfLife:
                 (row != cell[0] or col != cell[1]) and
                 (self.clist[row][col] == 1):
                     counter += 1
-        # PUT YOUR CODE HERE
         return counter
 
-    def update_cell_list(self, cell_list):
+    def update_cell_list(self, cell_list: List[List[int]]) -> List[List[int]]:
         """ Выполнить один шаг игры.
 
         Обновление всех ячеек происходит одновременно. Функция возвращает
@@ -122,7 +120,8 @@ class GameOfLife:
         """
         new_clist = [
             [0 for col in range(len(cell_list[0]))]
-            for row in range(len(cell_list))]
+            for row in range(len(cell_list))
+        ]
 
         for row in range(len(cell_list)):
             for col in range(len(cell_list[0])):
@@ -130,6 +129,5 @@ class GameOfLife:
                 (self.get_neighbours((row, col)) == 2) and
                 (self.clist[row][col] == 1):
                     new_clist[row][col] = 1
-        # PUT YOUR CODE HERE
         self.clist = new_clist
         return self.clist
